caae9511e53bb519c40e2cb239be3342f2c321ed
Yuto Tanaka <karaage_nennen@icloud.com>
2024-10-27 16:03:25 +0900

    wip

--- lua/plugins/diffview-nvim.lua
+++ lua/plugins/diffview-nvim.lua
@@ -1,15 +1,4 @@
 ---@type table
-local cmds = {
-    "DiffviewOpen",
-    "DiffviewFileHistory",
-    "DiffviewClose",
-    "DiffviewToggleFiles",
-    "DiffviewFocusFiles",
-    "DiffviewRefresh",
-    "DiffviewLog",
-}
-
----@type table
 local opts = {
     icons = {
         folder_closed = "",
@@ -22,14 +11,4 @@ local opts = {
     },
 }
 
----@type LazySpec
-local spec = {
-    "sindrets/diffview.nvim",
-    --lazy = false,
-    cmd = cmds,
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    opts = opts,
-    --cond = false,
-}
-
-return spec
+require("diffview").setup(opts)
