91e48391ec680690c03d17b1dc19a117f2268418
Yuto Tanaka <karaage_nennen@icloud.com>
2024-10-27 15:56:45 +0900

    wip

--- lua/plugins/context-menu-nvim.lua
+++ lua/plugins/context-menu-nvim.lua
@@ -1,12 +1,10 @@
 ---@type table
 local opts = require("plugins.configs.context-menu-nvim.opts")
 
-    "LintaoAmons/context-menu.nvim",
-    cmd = "ContextMenu",
-        local context_menu = require("context-menu")
-        context_menu.setup(opts)
+local context_menu = require("context-menu")
+context_menu.setup(opts)
 
-        -- usercmds
-        vim.api.nvim_create_user_command("ContextMenu", function()
-            context_menu.trigger_context_menu()
-        end, {})
+-- usercmds
+vim.api.nvim_create_user_command("ContextMenu", function()
+    context_menu.trigger_context_menu()
+end, {})
